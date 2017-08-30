e5b40315616cfb3bab8204d1e3c213ce8c77947c
Darko Stepanovic <darko.stepanovic@computerrock.com>
2017-08-30 15:04:47 +0200

    Update some html text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,11 +4,11 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center">About</h1>
-            <p>This is a weather application built on React. I have build this as a part of React course on Udemy</p>
+            <p>This is a weather application build on React. I have built this as a part of React course on Udemy</p>
             <p>Here are some tools I used:</p>
             <ul>
                 <li>
-                    <a href="https://facebook.github.io/react">React</a> - This was JS framework/library used.
+                    <a href="https://facebook.github.io/react">React</a> - This was JavaScript framework/library used.
                 </li>
                 <li>
                     <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map API to search weather data by city name.
