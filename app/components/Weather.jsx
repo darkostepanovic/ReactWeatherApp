4e1abc19ba3df984e73bb161483e5cfc00e1056f
Darko Stepanovic <darko.stepanovic@computerrock.com>
2017-08-30 14:55:02 +0200

    Add grid system and style examples

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -38,7 +38,7 @@ var Weather = React.createClass({
 
         return (
             <div>
-                <h3>Get Weather</h3>
+                <h3>Weather Component</h3>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
             </div>
